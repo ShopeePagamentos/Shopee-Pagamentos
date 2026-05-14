b53b63db8a1918752747b49fa90cf82b43477010
ShopeePagamentos <shopee.01pagamentos@gmail.com>
2026-05-14 16:50:34 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -30,56 +30,18 @@ function copiarPix(){
 }
 
 function finalizar(){
-
-    // cria tela de carregamento
-    let tela = document.createElement("div");
-
-    tela.innerHTML = `
-        <div class="loader"></div>
-        <p>Aguardando pagamento...</p>
-    `;
-
-    tela.style.position = "fixed";
-    tela.style.top = "0";
-    tela.style.left = "0";
-    tela.style.width = "100%";
-    tela.style.height = "100%";
-    tela.style.background = "#ffffff";
-    tela.style.display = "flex";
-    tela.style.flexDirection = "column";
-    tela.style.justifyContent = "center";
-    tela.style.alignItems = "center";
-    tela.style.zIndex = "999999";
-
-    // loading
-    let style = document.createElement("style");
-
-    style.innerHTML = `
-        .loader{
-            width:55px;
-            height:55px;
-            border:5px solid #f1f1f1;
-            border-top:5px solid #ff7a00;
-            border-radius:50%;
-            animation:girar 1s linear infinite;
-            margin-bottom:18px;
-        }
-
-        @keyframes girar{
-            100%{
-                transform:rotate(360deg);
-            }
-        }
-
-        .loader + p{
-            color:#000;
-            font-size:18px;
-            font-weight:600;
-            font-family:Arial,sans-serif;
-        }
-    `;
-
-    document.head.appendChild(style);
-    document.body.appendChild(tela);
-
+    window.location.href="https://wa.me/";
 }
+
+let t=3600;
+setInterval(()=>{
+    let m=Math.floor(t/60);
+    let s=t%60;
+
+    contador.innerText=`${m}m ${s}s`;
+    barraTempo.style.width=(t/3600)*100+"%";
+
+    t--;
+},1000);
+
+carregar();
